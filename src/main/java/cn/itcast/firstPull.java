51f5bc8e2a3c94479308e6b591cccbdcca51873d
admin <1300266575@qq.com>
2019-10-25 13:13:24 +0800

    删除test

--- src/main/java/cn/itcast/firstPull.java
+++
@@ -1,7 +0,0 @@
-package cn.itcast;
-
-public class firstPull {
-    public static void main(String[] args) {
-        System.out.println("test");
-    }
-}
--- src/main/java/cn/itcast/test.java
+++
@@ -1,7 +0,0 @@
-package cn.itcast;
-
-public class test {
-    public static void main(String[] args) {
-        System.out.println("test");
-    }
-}
